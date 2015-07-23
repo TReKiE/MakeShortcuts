310ee0c6d3bd05f611dbdd1738015ff27da71ffb
Jonathan Kay <git@jonathankay.com>
2015-07-23 02:38:02 -0400

    MakeShortcuts 1.0 checkin

---
+++ MakeShortcuts/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MakeShortcuts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.lblIntro = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Make";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Location = new System.Drawing.Point(35, 55);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(63, 13);
+            this.lblDest.TabIndex = 1;
+            this.lblDest.Text = "Destination:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(104, 52);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(422, 20);
+            this.txtFolder.TabIndex = 2;
+            // 
+            // lblIntro
+            // 
+            this.lblIntro.AutoSize = true;
+            this.lblIntro.Location = new System.Drawing.Point(167, 26);
+            this.lblIntro.Name = "lblIntro";
+            this.lblIntro.Size = new System.Drawing.Size(247, 13);
+            this.lblIntro.TabIndex = 4;
+            this.lblIntro.Text = "Makes 999 Notepad shortcuts in destination folder.";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(12, 95);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(563, 18);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bw
+            // 
+            this.bw.WorkerReportsProgress = true;
+            this.bw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_DoWork);
+            this.bw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_ProgressChanged);
+            this.bw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 169);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblIntro);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lblDest);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Make Notepad Shortcuts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label lblIntro;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker bw;
+    }
+}
+
---
+++ MakeShortcuts/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace MakeShortcuts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtFolder.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Shortcuts");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            bw.RunWorkerAsync();
+        }    
+
+        private void CreateShortcut(string name, int i)
+        {
+            string newfolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Notepad Executables");
+            object shDesktop = (object)"Desktop";
+
+            WshShell shell = new WshShell();
+            if (!Directory.Exists(txtFolder.Text)) Directory.CreateDirectory(txtFolder.Text);
+            string shortcutAddress = Path.Combine(txtFolder.Text,"Notepad" + i.ToString() + ".lnk");
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.TargetPath = Path.Combine(newfolder,name);
+            shortcut.Save();
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // set up Program Files folders for new notepad executables
+            string newfolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Notepad Executables");
+            string newfoldermui = Path.Combine(newfolder, "en-US");
+
+            // create folders if they don't exist
+            if (!Directory.Exists(newfolder)) Directory.CreateDirectory(newfolder);
+            if (!Directory.Exists(newfoldermui)) Directory.CreateDirectory(newfoldermui);
+
+            for (int i = 1; i < 1000; i++)
+            {
+                string name = "notepad";
+                name = name + i.ToString() + ".exe";
+
+                // copy files to new folders
+                System.IO.File.Copy("c:\\windows\\notepad.exe", Path.Combine(newfolder, name), true);
+                System.IO.File.Copy("c:\\windows\\en-us\\notepad.exe.mui", Path.Combine(newfoldermui, name + ".mui"), true);
+
+                // create shortcut for new notepad number
+                CreateShortcut(name, i);
+                bw.ReportProgress(50, "Notepad" + i.ToString() + " created");
+            }
+        }
+
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblStatus.Text = e.UserState.ToString();
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblStatus.Text = "Shortcut creation complete!";
+            button1.Enabled = true;
+        }
+    }
+}
